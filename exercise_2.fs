620f94f8f57278b1b61ce4f91d298956686bbb68
larcomicious <marco.gimena@student.ateneo.edu>
2025-03-11 19:07:16 +0800

    90% exercise 2 done

--- exercise_2.fs
+++ exercise_2.fs
@@ -28,12 +28,13 @@ mat2 rotate(float angle)
 void main()
 {
     // fragmentColor = vec4(shaderColor, 1.0f) * texture(shaderTexture, shaderTexCoord* vec2(-1.0f, 1.0f));
-    vec4 dispColor = texture(displacementMapA, shaderTexCoord + vec2(0.2f*t,-0.2f*t));
-    // vec4 dispColor = texture(displacementMapA, shaderTexCoord);
-    float lerpFact = ((dispColor.r * 2) - 1)*0.05;
-    vec2 disp = vec2(1, 1) * lerpFact;
+    // vec4 dispColor = texture(displacementMapA, shaderTexCoord + vec2(0.2f*t,-0.2f*t));
+    // // vec4 dispColor = texture(displacementMapA, shaderTexCoord);
+    // float lerpFact = ((dispColor.r * 2) - 1)*0.5;
+    // vec2 disp = vec2(1, 1) * lerpFact;
     // vec2 displacedCoord = clamp(shaderTexCoord + disp, 0.0, 1.0);
-    vec4 colorA = texture(shaderTextureA, shaderTexCoord + disp) * vec4(shaderColor, 1.0f);
+    // vec4 colorA = texture(shaderTextureA, shaderTexCoord + disp) * vec4(shaderColor, 1.0f);
+    vec4 colorA = texture(shaderTextureA, shaderTexCoord);
     // vec4 colorA = texture(shaderTextureA, displacedCoord);
     fragmentColor = colorA;
 }
